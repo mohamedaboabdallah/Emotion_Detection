75e179528c85ed9ecd6795bd7e074ef5467f614b
mohamedaboabdallah <mohamed541416@gmail.com>
2025-07-28 14:25:09 -0400

    error handling

--- EmotionDetection/emotion_detection.py
+++ EmotionDetection/emotion_detection.py
@@ -5,13 +5,16 @@ def emotion_detector(text_to_analyze):
     header = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
     obj = { "raw_document": { "text": text_to_analyze } }
     response = requests.post(url,json=obj,headers=header)
-    json_response=json.loads(response.text)
-    emotions = json_response['emotionPredictions'][0]['emotion']
-    dominant_emotion = max(emotions, key=emotions.get)
-    anger_score=emotions['anger']
-    disgust_score=emotions['disgust']   
-    fear_score=emotions['fear']
-    joy_score=emotions['joy']
-    sadness_score=emotions['sadness']
-    dominant_score=emotions['anger']
-    return {'anger': anger_score,'disgust': disgust_score,'fear': fear_score,'joy': joy_score,'sadness': sadness_score,'dominant_emotion': dominant_emotion }
+    if response.status_code == 200 :
+        json_response=json.loads(response.text)
+        emotions = json_response['emotionPredictions'][0]['emotion']
+        dominant_emotion = max(emotions, key=emotions.get)
+        anger_score=emotions['anger']
+        disgust_score=emotions['disgust']   
+        fear_score=emotions['fear']
+        joy_score=emotions['joy']
+        sadness_score=emotions['sadness']
+        dominant_score=emotions['anger']
+        return {'anger': anger_score,'disgust': disgust_score,'fear': fear_score,'joy': joy_score,'sadness': sadness_score,'dominant_emotion': dominant_emotion }
+    elif response.status_code == 400 or response.status_code == 500:
+        return {'anger': None,'disgust': None,'fear': None,'joy': None,'sadness': None,'dominant_emotion': None }
--- server.py
+++ server.py
@@ -9,19 +9,22 @@ def sent_detector():
     msg = request.args.get('textToAnalyze')
     print(msg)
     emotion_scores = emotion_detector(msg)
-    return(
-        f"For the given statement, the system response is "
-        f"'anger': {emotion_scores['anger']}, "
-        f"'disgust': {emotion_scores['disgust']}, "
-        f"'fear': {emotion_scores['fear']}, "
-        f"'joy': {emotion_scores['joy']} and "
-        f"'sadness': {emotion_scores['sadness']}. "
-        f"The dominant emotion is {emotion_scores['dominant_emotion']}."
-    )
+    if emotion_scores['anger'] == None:
+        return (" Invalid text!")
+    else:
+        return(
+            f"For the given statement, the system response is "
+            f"'anger': {emotion_scores['anger']}, "
+            f"'disgust': {emotion_scores['disgust']}, "
+            f"'fear': {emotion_scores['fear']}, "
+            f"'joy': {emotion_scores['joy']} and "
+            f"'sadness': {emotion_scores['sadness']}. "
+            f"The dominant emotion is {emotion_scores['dominant_emotion']}."
+        )
 
 @app.route("/")
 def render_index_page():
     return render_template("index.html")
 
 if __name__ == "__main__":
-    app.run(host = '0.0.0.0', port = 5000)
+    app.run(host = '0.0.0.0', port = 5001)
